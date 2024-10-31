afc2a57be96824554139ab3a2650ae07a84e638e
Santiago Guerra <santiago.guerra@mail.escuelaing.edu.co>
2024-10-31 14:40:14 -0500

    Primer commit. Scaffolding inicial del proyecto en Spring

---
+++ src/main/java/edu/eci/cvds/SearchLibrary/SearchLibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.SearchLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/SearchLibrary/SearchLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.SearchLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
